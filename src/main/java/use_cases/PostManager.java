e440e30dad6e927b9d9e83bf645c0f71e572c32f
sebastianibam002 <sebastianibam@gmail.com>
2021-10-14 12:35:55 -0400

    create some basic atribute to the class of postmanager, some methods without implementation while we define how we want to make it

--- src/main/java/use_cases/PostManager.java
+++ src/main/java/use_cases/PostManager.java
@@ -1,2 +1,33 @@
-package use_cases;public class PostManager {
+package use_cases;
+
+import java.util.ArrayList;
+
+/*
+store all the posts existing in the app,
+add a new post, comment/like a Post
+ */
+public class PostManager {
+    // TODO: Change String to Post when the entity is created
+    private ArrayList<String> posts;
+
+    public PostManager(){
+        this.posts = new ArrayList<String>();
+    }
+    //in case
+    public void commentPost(String post){
+        // TODO: Implement comment post
+    }
+
+    public void likePost(String post) {
+        // TODO: implement like post
+    }
+    public void addPostToList(String post){
+        this.posts.add(post);
+    }
+
+    public ArrayList<String> getPosts() {
+        return posts;
+    }
+
+
 }
