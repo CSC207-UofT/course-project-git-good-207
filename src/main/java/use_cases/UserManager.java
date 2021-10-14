9e5fbc1311def43aaadc6af5dacc17818322c586
notnine <g.jeremy@mail.utoronto.ca>
2021-10-14 11:45:06 -0400

    Made attributes private, added setter & getters, commented out UserProfilePresenter
    Changed the attributes in User to private and added setter and getter functions.
    Commented out UserProfilePresenter so that we can first discuss what exactly the presenters will do.

--- src/main/java/use_cases/UserManager.java
+++ src/main/java/use_cases/UserManager.java
@@ -12,51 +12,65 @@ public class UserManager {
      * Update User's username into username given
      */
     public void update_username(User user, String username) {
-        user.username = username;
+        user.setUsername(username);
     }
 
     /**
      * Update User's password into password given
      */
     public void update_password(User user, String new_password) {
-        user.password = new_password;
+        user.setPassword(new_password);
     }
 
     /**
      * Update User's bio into bio given
      */
     public void update_bio(User user, String bio) {
-        user.bio = bio;
+        user.setBio(bio);
     }
 
     /**
      * Add 1 like to the cuisine category that User liked
      */
     public void add_like(User user, String cuisine_category) {
-        int current_likes = user.like_history.get(cuisine_category);
+        int current_likes = user.getLike_history().get(cuisine_category);
         current_likes = current_likes + 1;
-        user.like_history.put(cuisine_category, current_likes);
+        user.getLike_history().put(cuisine_category, current_likes);
+//        Transforming this function into the one line below makes it less readable
+//        user.getLike_history().put(cuisine_category, user.getLike_history().get(cuisine_category) + 1);
     }
 
     /**
      * follow User want_to_follow, adding want_to_follow into the logged-in user's following list
      */
-    public String follow_user(User current_user, User want_to_follow) {
+    public boolean follow_user(User current_user, User want_to_follow) {
+        /**
+         * Return false if user is already followed, true otherwise.
+         */
         // check whether User want_to_follow is already followed by current_user
-        if (current_user.following.contains(want_to_follow)) {
-            return "User is already followed, following list remain unchanged";
+        if (current_user.getFollowing().contains(want_to_follow)) {
+            return false;
         }
         else {
             // update current_user's following list: following
-            current_user.following.add(want_to_follow);
-            return "User successfully followed";
+            current_user.getFollowing().add(want_to_follow);
+            return true;
         }
     }
 
     /**
      * add a follower to User's followers' list
      */
-    public void add_follower(User user, User new_follower){
-        user.followers.add(new_follower);
+    public boolean add_follower(User user, User new_follower){
+        /**
+         * Return false if new_follower is already following user, true otherwise.
+         */
+        if (user.getFollowers().contains(new_follower)){
+            return false;
+        }
+        else {
+            user.getFollowers().add(new_follower);
+            return true;
+        }
     }
 }
