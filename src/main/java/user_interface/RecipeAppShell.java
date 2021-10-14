50fc6ad32ac414d56e8879ccffc292cc92fe0479
sebastianibam002 <sebastianibam@gmail.com>
2021-10-14 10:54:41 -0400

    created a basic while loop, in order to get the input from the user

--- src/main/java/user_interface/RecipeAppShell.java
+++ src/main/java/user_interface/RecipeAppShell.java
@@ -1,6 +1,30 @@
 package user_interface;
+
+import java.util.Scanner;
+
 /*
     provides a command line user interface for input/output
  */
 public class RecipeAppShell {
+    final static String GREETING = "Hello! Welcome to recipe book\n" +
+            "Login (a)\n" +
+            "Forgot my password (b)\n" +
+            "Create an account (c)\n" +
+            "Settings (d)";
+    public static void main(String[] args) {
+        boolean run = true;
+        System.out.println(GREETING);
+        while(run){
+            //main loop in which tha program will run
+            Scanner scan = new Scanner(System.in);
+            String val = scan.nextLine();
+            System.out.println(val);
+            // input validation, in case we need it
+            if(val.equals("exit")){
+                run = false;
+            }
+
+
+        }
+    }
 }
