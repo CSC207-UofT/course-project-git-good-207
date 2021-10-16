2a121ab5bdbace6c008c7528e04b1125469bb95f
notnine <g.jeremy@mail.utoronto.ca>
2021-10-16 08:51:31 -0400

    Added: createUser, runBrowseProfile
    CHANGES:
    1.
    RecipeAppController.java
    
    line 78 (at the time of change)
    else if (action == ShellAction.CUSTOMIZEPROFILE) {
                this.userProfilePresenter.run(ShellAction.POST);
    
    changed to
    
    else if (action == ShellAction.CUSTOMIZEPROFILE) {
                this.userProfilePresenter.run(ShellAction.CUSTOMIZEPROFILE);
    
    There's just a small typo here
    
    2. added the function createUser in UserManager that returns a User with the username and password attributes
    
    3. Created function in runBrowseProfile inside UsserProfilePresenter to view given user's username, password, bio

--- src/main/java/controllers/UserProfilePresenter.java
+++ src/main/java/controllers/UserProfilePresenter.java
@@ -7,32 +7,52 @@ import entities.InOut;
 import entities.ShellAction;
 import entities.User;
 import entities.Post;
+import use_cases.LoginManager;
 import use_cases.UserManager;
 
 public class UserProfilePresenter {
     private InOut inOut;
+    /*private String username; // the username that currentUser wants to view the profile of*/
 
+    // Constructors
     public UserProfilePresenter(InOut inOut) {
         this.inOut = inOut;
     }
 
+//    public UserProfilePresenter(InOut inOut, String username) {
+//        this.inOut = inOut;
+//        this.username = username;
+//    } For when we have the database manager
+
+
+
     public void run(ShellAction action) {
         // TODO: get user inputs/display info based on the given action
+        if (action == ShellAction.BROWSEPROFILE) {
+            // Browse user's profile, given String username:
+            // run a helper method (getUserToBrowse) to call Database manager to get the user object given the username
+            // run a helper method called runBrowseProfile given User object to browse the user's profile
+        }
+
+        else if (action == ShellAction.CUSTOMIZEPROFILE) {
+            // Customize user's profile:
+            // 1. Display current user's profile
+            // 2. Ask current user on what they want to customize: username, bio, password
+            // 3. Customize one of the three: username, bio, or password
+            // run a helper method called runBrowseProfile given current user to browse current user's profile
+            // run a helper method called runCustomizeProfile
+//            /*this.runBrowseProfile(LoginManager.getCurrUser());*/
+//            A Non-static method cannot be referenced from a static context error
+        }
     }
-//    public static void main(String[] args) {
-//
-//        // initialize some empty variables to create a new object User
-//        HashMap<String, Integer> like_history = new HashMap<String, Integer>();
-//        ArrayList<User> followers = new ArrayList<User>();
-//        ArrayList<User> following = new ArrayList<User>();
-//        ArrayList<Post> posts = new ArrayList<Post>();
-//
-//        // create new User current_user
-//        User current_user = new User("Glen", "letmein", "Aspriring CS Student",
-//                like_history, followers, following, posts);
-//        /**
-//         * Update username by calling the update_username function in UserManager
-//         */
-//        // write a function here that update username
-//    }
+
+    /**
+     * views the information(attributes) of user
+     */
+    public String runBrowseProfile(User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        String bio = user.getBio();
+        return ("Username: " + username + "\nPassword: " + password + "\nBio: " + bio);
+    }
 }
