443e942cdee7d7b3cfc916fe5336538ffa0c649e
sebastianibam002 <sebastianibam@gmail.com>
2021-10-16 18:32:28 -0400

    change string to post and modify the comment and like post

--- src/main/java/use_cases/PostManager.java
+++ src/main/java/use_cases/PostManager.java
@@ -1,5 +1,9 @@
 package use_cases;
 
+import entities.Comment;
+import entities.Post;
+import entities.User;
+
 import java.util.ArrayList;
 
 /*
@@ -7,25 +11,51 @@ store all the posts existing in the app,
 add a new post, comment/like a Post
  */
 public class PostManager {
-    // TODO: Change String to Post when the entity is created
-    private ArrayList<String> posts;
+    private ArrayList<Post> posts;
 
     public PostManager(){
-        this.posts = new ArrayList<String>();
+        this.posts = new ArrayList<Post>();
     }
     //in case
-    public void commentPost(String post){
-        // TODO: Implement comment post
+    /*
+        Returns true if the interaction could be done, false otherwise
+        if the type is true, then the interaction is a comment
+     */
+    private boolean interactPost(Post post, Object obj, boolean type){
+        // it looks into all the post in the stored structure
+        // in order to work, the comparison of post should be
+        // implemented
+        for(int i = 0; i < this.posts.size(); i++)
+        {
+            if(this.posts.get(i).equals(post))
+            {
+                if(type)
+                {
+                    this.posts.get(i).addComment((Comment) obj);
+                }
+                else
+                {
+                    this.posts.get(i).addLike((User) obj);
+                }
+                return true;
+
+            }
+        }
+        return false;
     }
 
-    public void likePost(String post) {
-        // TODO: implement like post
+    public boolean commentPost(Post post, Comment comment){
+        return interactPost(post, comment, true);
     }
-    public void addPostToList(String post){
+
+    public boolean likePost(Post post, User user) {
+        return interactPost(post, user, false);
+    }
+    public void addPostToList(Post post){
         this.posts.add(post);
     }
 
-    public ArrayList<String> getPosts() {
+    public ArrayList<Post> getPosts() {
         return posts;
     }
 
