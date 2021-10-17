b62a3097602be91013f6a7db1db1a0f29ea48c8c
Eric <>
2021-10-16 20:26:04 -0400

    Implement DatabaseManager with dummy data

--- src/main/java/use_cases/DatabaseManager.java
+++ src/main/java/use_cases/DatabaseManager.java
@@ -1,55 +1,47 @@
 package use_cases;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import entities.Post;
+import entities.Recipe;
+import entities.User;
+import java.time.LocalDateTime;
 
 public class DatabaseManager {
-    // Database for the recipe posts and their comments
-    private String recipefileName;
-    // Database for the usernames and passwords (and maybe their unique ids if they change usernames)
-    private String userFileName;
 
-    //TODO: Get a User object to check its username and password to help verify the login.
-    public entities.User getUser() {
+    public DatabaseManager() {
 
     }
 
-    //TODO: Add a new User into the database with its username and password
-    public boolean addUser() {
-
+    public Post[] getAllPosts() {
+        Post chinesePost = new Post(
+                new User("justin", "password"),
+                LocalDateTime.now(),
+                new Recipe("Stir Fry"),
+                "Chinese");
+        Post koreanPost = new Post(
+                new User("sebastian", "password"),
+                LocalDateTime.now(),
+                new Recipe("Bibimbap"),
+                "Korean");
+        Post italianPost = new Post(
+                new User("glen", "password"),
+                LocalDateTime.now(),
+                new Recipe("Gelato"),
+                "Italian");
+        Post cursedPost = new Post(
+                new User("shawn", "password"),
+                LocalDateTime.now(),
+                new Recipe("Peanut Butter and Onion Sandwich"),
+                "Other");
+        Post[] dummyPostArray = {chinesePost, koreanPost, italianPost, cursedPost};
+        return dummyPostArray;
     }
 
-    public List<String> readFile(String fileName) {
-        List<String> list = new ArrayList<>();
-
-        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            list = stream
-                    .filter()
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading " + fileName);
-            e.printStackTrace();
-        }
-
-        return list;
-    }
-
-    public boolean writeToFile(String fileName) {
-        try {
-            FileWriter writer = new FileWriter("");
-            writer.write("");
-            writer.close();
-            return true;
-        } catch (IOException e) {
-            System.out.println("An error occurred while writing to " + fileName);
-            e.printStackTrace();
-        }
-        return false;
+    public User[] getAllUsers() {
+        User justin = new User("justin", "pass");
+        User sebastian = new User("sebastian", "pass");
+        User glen = new User("glen", "pass");
+        User shawn = new User("shawn", "pass");
+        User[] dummyAllUsers = {justin, sebastian, glen, shawn};
+        return dummyAllUsers;
     }
 }
