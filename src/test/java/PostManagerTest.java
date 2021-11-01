acba94c98d5b34abe7706068a59f0550dc98096d
sebastianibam002 <sebastianibam@gmail.com>
2021-11-01 01:07:30 -0400

    created the file for testing the use case post manager and started adding some comments

--- src/test/java/PostManagerTest.java
+++ src/test/java/PostManagerTest.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PostManagerTest {
+import entities.Ingredient;
+import entities.Post;
+import entities.Recipe;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import use_cases.PostManager;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class PostManagerTest {
+    private PostManager postManager;
+
+    @BeforeEach
+    void setupPostManagerTest(){
+        // set up a list of predefined posts
+        ArrayList<Post> listPosts = new ArrayList<>();
+        ArrayList<Ingredient> ingredient = new ArrayList<>();
+        ArrayList<String> steps = new ArrayList<>();
+        steps.add("Add Cheese into the oven");
+        ingredient.add(new Ingredient("Cheese"));
+        listPosts.add(new Post("ibarrame", LocalDateTime.now(), new
+                Recipe("Recipe Test", ingredient, steps), "Italian"));
+
+        this.postManager = new PostManager(listPosts);
+    }
 }
