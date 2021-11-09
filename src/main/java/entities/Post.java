ab7a13b7d1ae911e9e212e99c2d868dcd8e6bc5f
sebastianibam002 <sebastianibam@gmail.com>
2021-11-08 20:47:04 -0600

    revert to pass as parameter the post id

--- src/main/java/entities/Post.java
+++ src/main/java/entities/Post.java
@@ -13,16 +13,13 @@ public class Post extends PostableItem {
     private ArrayList<Comment> comments;
     private Recipe recipe;
     private final String category;
-    private final String id;
 
-    public Post(String authorId, LocalDateTime postedTime, Recipe recipe, String category) {
-        super(authorId, postedTime);
+    public Post(String authorId, LocalDateTime postedTime, Recipe recipe, String category, String id) {
+        super(authorId, postedTime, id);
         this.likedUsers = new ArrayList<>();
         this.comments = new ArrayList<>();
         this.recipe = recipe;
         this.category = category;
-        // assigns a random new value each time
-        this.id = UUID.randomUUID().toString();
     }
 
     @Override
