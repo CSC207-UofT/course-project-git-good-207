afd107c543b861440b7fe7ac39118c8a8ebad300
Shawn Plotko <shawn.plotko@mail.utoronto.ca>
2021-11-10 16:39:59 -0500

    LoginControllerTest changes

--- src/test/java/controllers/LoginControllerTest.java
+++ src/test/java/controllers/LoginControllerTest.java
@@ -1,14 +1,9 @@
 package controllers;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import use_cases.LoginManager;
 import user_interface.RecipeAppInOut;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,14 +13,6 @@ class LoginControllerTest {
     RecipeAppInOut inOut= new RecipeAppInOut();
     LoginController loginController = new LoginController(inOut, loginManager);
 
-    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-    private final InputStream originalIn = System.in;
-
-    @BeforeAll
-    static void setup(){
-        System.setOut(new PrintStream(outContent));
-    }
 
     @Test
     void testRun() {
@@ -36,21 +23,6 @@ class LoginControllerTest {
         assertNull(loginManager.getCurrUser());
     }
 
-    @Test
-    void testRunWelcomePage(){
-        //String welcomeActionChoice = "0";
-        //String signUpUsernameInput = "shawn";
-        //String signUpPasswordInput = "1234";
-        //ByteArrayInputStream inContent = new ByteArrayInputStream((welcomeActionChoice + System.lineSeparator() + signUpUsernameInput + System.lineSeparator() + signUpPasswordInput).getBytes());
-
-
-        loginController.runWelcomePage();
-        //System.setIn(inContent);
-        //assertTrue(outContent.toString().contains("Sign up successful."));
-
-
-
-
-    }
 
 }
+
