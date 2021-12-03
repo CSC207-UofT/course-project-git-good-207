6bd254d4cdebbb6368643d4ac8c728b9a04c8eee
Eric <>
2021-12-03 07:20:08 -0500

    Create post UI

--- foodstagram-ui/src/app/app-routing.module.ts
+++ foodstagram-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { FeedComponent } from './feed/feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FeedComponent },
+  { path: 'create-post', component: CreatePostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- foodstagram-ui/src/app/app.module.ts
+++ foodstagram-ui/src/app/app.module.ts
@@ -7,18 +7,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FeedComponent } from './feed/feed.component';
 import { MatCardModule } from '@angular/material/card';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FeedComponent
+    FeedComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
