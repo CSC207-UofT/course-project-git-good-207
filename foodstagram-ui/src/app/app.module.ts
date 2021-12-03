c1b9442e0dc2fde34481f7a5d80486c6a1f6b496
Eric <>
2021-12-03 07:49:38 -0500

    User search bar

--- foodstagram-ui/src/app/app.module.ts
+++ foodstagram-ui/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- foodstagram-ui/src/app/navbar/navbar.component.ts
+++ foodstagram-ui/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  myControl = new FormControl();
+  users: string[] = ['Eric', 'Shawn', 'Yolanda'];
+  filteredUsers: Observable<string[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.filteredUsers = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
   }
 
+  ngOnInit(): void {}
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.users.filter(user => user.toLowerCase().includes(filterValue));
+  }
 }
