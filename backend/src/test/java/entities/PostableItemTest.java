c3b80bb49889157edff51c475085d86e8a1a0fb7
Shawn Plotko <shawn.plotko@mail.utoronto.ca>
2021-12-03 21:48:00 -0500

    Add PostableItemTest

---
+++ backend/src/test/java/entities/PostableItemTest.java
@@ -0,0 +1,32 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostableItemTest {
+    private final LocalDateTime dateTime = LocalDateTime.now();
+    private final PostableItem pItem = new Comment("test", "1234", dateTime, "321" );
+
+    @Test
+    void getAuthorId() {
+        String actual = pItem.getAuthorId();
+        String expected = "1234";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getId() {
+        String actual = pItem.getId();
+        String expected = "321";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getCreatedTime() {
+        LocalDateTime actual = pItem.getCreatedTime();
+        assertEquals(this.dateTime, actual);
+    }
+}
